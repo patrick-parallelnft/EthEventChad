fb0d9c4b5de762102517186d834bb4b7eda0d7ba
Patrick Pu <patrick@parallelnft.com>
2022-10-01 15:28:09 -0400

    add some formatting

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits, bold } = require("discord.js");
 const { parallel_alpha_contract } = require("./alchemy");
+const cards = require("./data/cards.json");
+const R = require("ramda");
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
@@ -16,27 +18,39 @@ client.login(process.env.DISCORD_TOKEN);
 
 async function listenToTransferEvents() {
   const channel = await client.channels.fetch(process.env.CHANNEL_TEST_BOT);
+
   parallel_alpha_contract.on(
     "TransferBatch",
-    (operartor, from, to, ids, values) => {
-      channel.send(`TransferBatch Event
-        operator ${operartor}
-        from ${from}
-        to ${to}
-        ids ${ids}
-        values ${values}`);
+    (operartor, from, to, ids, values, hash) => {
+      channel.send(
+        `${bold("Transfer batch:")}
+${bold(`${R.sum(values)} cards:`)} 
+${ids
+  .map((id, i) => `${values[i]} ${cards[id].name}`)
+  .join(", ")} have been transfered.
+${bold(`From:`)} ${from}
+${bold(`To:`)} ${to}
+${bold("Operator:")} ${operartor}
+${bold("Hash:")} ${hash.toString()}`
+      );
     }
   );
 
   parallel_alpha_contract.on(
     "TransferSingle",
-    (operartor, from, to, id, value) => {
-      channel.send(`TransferSingle Event
-        operator ${operartor}
-        from ${from}
-        to ${to}
-        id ${id}
-        value ${value}`);
+    (operartor, from, to, id, value, hash) => {
+      channel.send(
+        `${bold("Transfer single:")}
+${bold(`${value} card:`)}
+${cards[id].name} has been transfered. 
+${bold(`From:`)} ${from}
+${bold(`To:`)} ${to}
+${bold("Operator:")} ${operartor}
+${bold("Hash:")} ${hash.toString()}`,
+        {
+          files: [cards[id]?.image],
+        }
+      );
     }
   );
 }
