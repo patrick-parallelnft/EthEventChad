0f46ac4841523931181d489b9e528a458a08ed00
Patrick Pu <patrick@parallelnft.com>
2022-10-02 14:54:27 -0400

    add a webhook

--- main.js
+++ main.js
@@ -8,9 +8,16 @@ const {
 
 const express = require("express");
 const app = express();
+app.use(express.json());
 const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.send("live"));
+
+app.post("/webhook", async (req, res) => {
+  const channel = await client.channels.fetch(process.env.CHANNEL_WEBHOOK);
+  channel.send(`Webhook body: ${JSON.stringify(req.body)}`);
+  res.send(req.body);
+});
 
 app.listen(port, () =>
   console.log(`Eth Event Chad listening on port ${port}!`)
